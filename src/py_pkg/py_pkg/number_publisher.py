c0f1933fbee26c2de80fa033e4e6b26c0b0327db
heqikun1511 <3669308367@qq.com>
2026-06-03 16:35:21 +0800

    init ros2_ws

---
+++ src/py_pkg/py_pkg/number_publisher.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+import rclpy
+from std_msgs.msg import Int32
+
+
+class NumberPublisher(Node):
+    def __init__(self):
+        super().__init__("number_publisher")
+        self.declare_parameter("number", 2)
+        self.declare_parameter("timer_period", 1.0)
+        self.number_= self.get_parameter("number").value
+        self.publisher_ = self.create_publisher(Int32, "number", 10)
+        timer_period = self.get_parameter("timer_period").value
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.get_logger().info("Number Publisher has been started.")
+
+    def timer_callback(self):
+        msg = Int32()
+        msg.data = self.number_
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Publishing: {msg.data}")
+        self.i += 1
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = NumberPublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()    
+
+
+
+if __name__=="__main__":
+    main()
